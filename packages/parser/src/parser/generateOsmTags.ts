ee3634dade102bb85208487d2aff3db22040b03f
Kyle Hensel <k.y.l.e@outlook.co.nz>
2024-04-16 19:48:04 +1000

    sanity check that `:period` = ∑ of `:sequence`

--- packages/parser/src/parser/generateOsmTags.ts
+++ packages/parser/src/parser/generateOsmTags.ts
@@ -353,6 +353,25 @@ export function generateOsmTags(
     ' ',
   );
 
+  // sanity check that the period = ∑ of the sequence
+  for (const key in tags) {
+    const sequenceKey = key.replace(':period', ':sequence');
+    if (key.endsWith(':period') && tags[sequenceKey]) {
+      const period = +tags[key];
+      const sequence = tags[sequenceKey]
+        .replaceAll(/[()]/g, '')
+        .split('+')
+        .map(Number);
+      const sequenceSum = sequence.reduce((a, b) => a + b, 0);
+      if (period !== sequenceSum) {
+        warnings.push({
+          type: 'period_sum',
+          value: `The ${key.split(':')[1]} sequence “${tags[sequenceKey]}” does not add up to ${period}.`,
+        });
+      }
+    }
+  }
+
   return {
     tags: sortObject(deleteUndefinedKeys(stripProxy(tags))),
     ialaId,
