dae543a7e1e2f36b465dd1810f4d0a6009f77ebe
Kyle Hensel <k.y.l.e@outlook.co.nz>
2026-09-22 00:41:52 +0800

    白屏了

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '', // use relative paths
+  base: '/',
   plugins: [react()],
   server: {
     port: 5123,
